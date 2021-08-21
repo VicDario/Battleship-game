59652736d0f97eef5fe8147a75623d85b55ec483
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-20 20:58:45 -0400

    Menu and Game imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { useState } from 'react';
+import Menu from './components/Menu';
+import Game from './components/Game'
 
 function App() {
   let [toPlay, setToPlay] = useState(false);
   return (
-    
+    <div className="container">
+      {toPlay ? <Game /> : <Menu setToPlay={setToPlay} />}
+    </div>
   );
 }
 
