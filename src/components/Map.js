dc77d7c5325bcae1be601ee6a4f62c9fc90c4d15
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-21 00:54:44 -0400

    Changes in development

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,71 +1,35 @@
 const Map = (props) => {
+    const preview = (event) => {
+        event.target.style.backgroundColor = "#ff8080";
+    };
 
-    const previsualization = (event) => {
-        event.target.style.backgroundColor = '#ff8080';
-        console.log(event);
-    }
+    const removePreview = (event) => {
+        event.target.style.backgroundColor = "#ffffff";
+    };
 
-    const removePrevisualization = (event) => {
-        event.target.style.backgroundColor = '#ffffff';
-    }
-
-    const putBoat = (event) => {
-        
-    }
-
+    const fireTorpedo = (event, row, cell) => {};
     return (
         <>
-        {   
-            props.areReady ?
-                props.player.map.map((row, rowIndex) => {
-                    return (
-                        <div className="row" key={rowIndex} value={row}>
-                            {
-                                row.map((cell, cellIndex) => {
-                                    return (
-                                        <div 
-                                            className="cell" 
-                                            key={cellIndex} 
-                                            onMouseOver={previsualization} 
-                                            onMouseLeave={removePrevisualization}
-                                        >
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    )
-                })
-            :
-                <>
-                <h1>{`${props.player.name} preparation`}</h1>
-                {   
-                    props.player.map.map((row, rowIndex) => {
-                        return ( 
-                            <div className="row" key={rowIndex}>
-                                {
-                                    row.map((cell, cellIndex) => {
-                                        return (
-                                            <div 
-                                                className="cell" 
-                                                key={cellIndex} 
-                                                onMouseOver={previsualization} 
-                                                onMouseLeave={removePrevisualization}
-                                                onClick={putBoat}
-                                            >
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                        )
-                    })
-                }
-                </>
-                
-        }
+            <h1>{`${props.player.name} preparation`}</h1>
+            {props.player.map.map((row, rowIndex) => {
+                return (
+                    <div className="row" key={rowIndex}>
+                        {row.map((cell, cellIndex) => {
+                            return (
+                                <div
+                                    className="cell"
+                                    key={cellIndex}
+                                    onMouseOver={preview}
+                                    onMouseLeave={removePreview}
+                                    onClick={(event) => fireTorpedo(event, rowIndex, cellIndex)}
+                                ></div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
         </>
     );
-}
+};
 
 export default Map;
