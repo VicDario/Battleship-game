aa5aad777c24461371368b2c04c1d1aa15f6cb1a
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-21 01:58:58 -0400

    Changes for the new strategy and fixes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 const Map = (props) => {
     const preview = (event) => {
         event.target.style.backgroundColor = "#b4b3b3";
@@ -8,26 +10,26 @@ const Map = (props) => {
     };
 
     const fireTorpedoPlayer = (event, row, cell) => {
-        let map = props.player.map;
+        let map = props.enemy.map;
         if (map[row][cell] === 1)   map[row][cell] = 2;
         else if (map[row][cell] === 2 || map[row][cell] === 3)  return;
         else map[row][cell] = 3;
 
-        props.setPlayer(Object.assign(props.player, map));
+        props.setEnemy(Object.assign(props.enemy, map));
         props.setTurn(!props.turn)
     };
 
     return (
         <>
             <h1>{`${props.player.name} turn`}</h1>
-            {props.player.map.map((row, rowIndex) => {
+            {props.enemy.map.map((row, rowIndex) => {
                 return (
                     <div className="row" key={rowIndex}>
                         {row.map((cell, cellIndex) => {
                             let background;
-                            if (cell === 1 || cell === 2) background = '';
+                            if (cell === 1 || cell === 0) background = '';
                             else if (cell === 2) background = 'green';
-                            else if (cell === 3) background = 'green';
+                            else if (cell === 3) background = 'red';
                             return (
                                 <div
                                     className={`cell ${background}`}
