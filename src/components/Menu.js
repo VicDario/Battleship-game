de51d4d5a38aca83d11ab63f3373ba6f4c878b65
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-20 22:21:46 -0400

    Menu completed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,16 @@
-const Menu = (props) => {
+import { IoBoat } from 'react-icons/io5';
+import { AiFillHeart } from 'react-icons/ai';
 
+const Menu = (props) => {
     const goToGame = () => {
-        
+        props.setToPlay(true);
     }
     
     return (
         <div className='container'>
-            <h1 className="title">Battleship</h1>
+            <h1 className="title"><IoBoat />BATTLESHIP<IoBoat /></h1>
             <button className="btn-play" onClick={goToGame}>Play</button>
+            <p className="footer">Made with <AiFillHeart /></p>
         </div>
     );
 
