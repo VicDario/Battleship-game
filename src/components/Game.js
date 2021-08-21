9ac60e8463cd71104b3b48cc0fae703f15d3c580
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-21 01:49:01 -0400

    Changes in strategy

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Map from './Map';
 
 const Game = (props) => {
-    const generateMap = (n) => {
+    /* const generateMap = (n) => {
         // Generate a map of size n x n
         let map = [];
         for (let i = 0; i < n; i++) {
@@ -12,38 +12,78 @@ const Game = (props) => {
             }
         }
         return map;
-    }
+    } */
     
-    class Boat {
+    /* class Boat {
         constructor(size) {
           this.size = size;
           this.positions = [];
         }
-      }
+    } */
     
     class Player {
-        constructor(name, boats, map) {
+        constructor(name, map) {
             this.name = name;
-            this.boats = boats;
             this.map = map;
-            this.boatsPlaced = 0;
         }
     }
+    let gameBoard1 = [
+        [1,1,1,1,1,0,0,0,0,1],
+        [0,0,0,0,0,0,0,0,0,1],
+        [0,0,0,0,0,0,0,0,0,1],
+        [0,0,0,0,0,0,0,0,0,1],
+        [0,0,0,0,0,0,0,0,0,0],
+        [1,0,0,1,1,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,0,0,0,0,0,0]
+    ];
+
+    let gameBoard2 = [
+        [0,0,0,0,0,0,0,0,0,1],
+        [0,1,1,0,0,1,0,0,0,1],
+        [0,0,0,0,0,1,0,0,0,1],
+        [0,0,0,0,0,1,0,0,0,1],
+        [0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0],
+        [1,0,0,0,0,0,1,1,1,1],
+        [0,0,0,0,0,0,0,0,0,0]
+    ];
 
     let [turn, setTurn] = useState(true);
     let [areReady, setAreReady] = useState(false);
-    let boats = [new Boat(2), new Boat(3), new Boat(4), new Boat(4), new Boat(5)];
+    /* let boats = [new Boat(2), new Boat(3), new Boat(4), new Boat(4), new Boat(5)]; */
 
-    let [player, setPlayer] = useState(new Player('Player', boats, generateMap(10)));
-    let [computer, setComputer] = useState(new Player('Computer', boats, generateMap(10)));
+    let [player, setPlayer] = useState(new Player('Player', gameBoard1));
+    let [computer, setComputer] = useState(new Player('Computer', gameBoard2));
 
     return (
         <div className="container">
             {
                 turn ?
-                    <Map player={player} setPlayer={setPlayer} areReady={areReady} turn={turn} setTurn={setTurn} /> 
+                    <Map 
+                        player={player} 
+                        enemy={computer} 
+                        setPlayer={setPlayer} 
+                        setEnemy={setComputer} 
+                        areReady={areReady} 
+                        turn={turn} 
+                        setTurn={setTurn} 
+                    /> 
                 :
-                    <Map player={computer} setPlayer={setComputer} areReady={areReady} setAreReady={setAreReady} turn={turn} setTurn={setTurn} />
+                    <Map 
+                        player={computer} 
+                        enemy={player} 
+                        setPlayer={setComputer} 
+                        setEnemy={setPlayer}
+                        areReady={areReady} 
+                        setAreReady={setAreReady} 
+                        turn={turn} 
+                        setTurn={setTurn} 
+                    />
             }
         </div>
     );
