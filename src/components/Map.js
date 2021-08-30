8dfe8a043bad078968202908a504718340e55873
VicDario <26585356+VicDario@users.noreply.github.com>
2021-08-29 23:47:01 -0400

    Preview of the torpedo added

--- src/components/Map.js
+++ src/components/Map.js
@@ -15,7 +15,7 @@ const Map = (props) => {
 
                         props.iaMemory['lastFire'] = fire;
                         if ( fire === 'hit' ) {
-                            props.iaMemory['cordenate'] = [row, cell];
+                            props.iaMemory['coordinates'] = [row, cell];
                             props.iaMemory['direction'] = 'up';
                             doesComputerHaveToPlay = false;
                         } else if ( fire === 'miss' ) {
@@ -28,15 +28,15 @@ const Map = (props) => {
                         if( props.iaMemory['lastFire'] === 'hit' ) {
                             // Now check directions to fire, to try hit another parts of the boats
                             if( props.iaMemory['direction'] === 'up' ) {
-                                if( props.iaMemory['cordenate'][0] - 1  < 0 ){
+                                if( props.iaMemory['coordinates'][0] - 1  < 0 ){
                                     props.iaMemory['direction'] = 'right';
                                     continue;
                                 }
-                                let row = props.iaMemory['cordenate'][0] - 1;
-                                let cell = props.iaMemory['cordenate'][1];
+                                let row = props.iaMemory['coordinates'][0] - 1;
+                                let cell = props.iaMemory['coordinates'][1];
                                 const fire = fireTorpedoIA(row, cell);
                                 if (fire === 'hit') {
-                                    props.iaMemory['cordenate'] = [row, cell];
+                                    props.iaMemory['coordinates'] = [row, cell];
                                     doesComputerHaveToPlay = false;
                                 }else if ( fire === 'miss' ) {
                                     props.iaMemory['row'] = false;
@@ -47,15 +47,15 @@ const Map = (props) => {
 
                             }
                             if( props.iaMemory['direction'] === 'right' ) {
-                                if( props.iaMemory['cordenate'][1] + 1  >= props.enemy.map[0].length ) {
+                                if( props.iaMemory['coordinates'][1] + 1  >= props.enemy.map[0].length ) {
                                     props.iaMemory['direction'] = 'down';
                                     continue;
                                 }
-                                let row = props.iaMemory['cordenate'][0];
-                                let cell = props.iaMemory['cordenate'][1] + 1;
+                                let row = props.iaMemory['coordinates'][0];
+                                let cell = props.iaMemory['coordinates'][1] + 1;
                                 const fire = fireTorpedoIA(row, cell);
                                 if (fire === 'hit') {
-                                    props.iaMemory['cordenate'] = [row, cell];
+                                    props.iaMemory['coordinates'] = [row, cell];
                                     doesComputerHaveToPlay = false;
                                 }else if ( fire === 'miss' ) {
                                     props.iaMemory['row'] = false;
@@ -65,15 +65,15 @@ const Map = (props) => {
                                 }
                             }
                             if( props.iaMemory['direction'] === 'down' ) {
-                                if( props.iaMemory['cordenate'][0] + 1  >= props.enemy.map.length ) {
+                                if( props.iaMemory['coordinates'][0] + 1  >= props.enemy.map.length ) {
                                     props.iaMemory['direction'] = 'left';
                                     continue;
                                 }
-                                let row = props.iaMemory['cordenate'][0] + 1;
-                                let cell = props.iaMemory['cordenate'][1];
+                                let row = props.iaMemory['coordinates'][0] + 1;
+                                let cell = props.iaMemory['coordinates'][1];
                                 const fire = fireTorpedoIA(row, cell);
                                 if (fire === 'hit') {
-                                    props.iaMemory['cordenate'] = [row, cell];
+                                    props.iaMemory['coordinates'] = [row, cell];
                                     doesComputerHaveToPlay = false;
                                 }else if ( fire === 'miss' ) {
                                     props.iaMemory['row'] = false;
@@ -83,16 +83,16 @@ const Map = (props) => {
                                 }
                             }
                             if( props.iaMemory['direction'] === 'left' ) {
-                                if( props.iaMemory['cordenate'][1] - 1  < 0 ) {
+                                if( props.iaMemory['coordinates'][1] - 1  < 0 ) {
                                     props.iaMemory['lastFire'] = false
                                     props.iaMemory['direction'] = false;
-                                    props.iaMemory['cordenate'] = []
+                                    props.iaMemory['coordinates'] = []
                                 }
-                                let row = props.iaMemory['cordenate'][0];
-                                let cell = props.iaMemory['cordenate'][1] - 1;
+                                let row = props.iaMemory['coordinates'][0];
+                                let cell = props.iaMemory['coordinates'][1] - 1;
                                 const fire = fireTorpedoIA(row, cell);
                                 if (fire === 'hit') {
-                                    props.iaMemory['cordenate'] = [row, cell];
+                                    props.iaMemory['coordinates'] = [row, cell];
                                     doesComputerHaveToPlay = false;
                                 }else if ( fire === 'miss' ) {
                                     props.iaMemory['lastFire'] = false;
@@ -110,15 +110,19 @@ const Map = (props) => {
         }
         // eslint-disable-next-line
     });
-    
+    // Paint a cell of the map
     const preview = (event) => {
-        event.target.style.backgroundColor = "#b4b3b3";
+        event.target.classList.contains('green') || event.target.classList.contains('red')
+        ? event.target.style.backgroundColor = ''
+        : event.target.style.backgroundColor = '#b4b3b3';
     };
 
+    // 
     const removePreview = (event) => {
         event.target.style.backgroundColor = "";
     };
 
+    // Check if the map doesn't have boats
     const checkWin = (map) => {
         for (let row = 0; row < map.length; row++) {
             for (let cell = 0; cell < map[row].length; cell++) {
@@ -129,14 +133,25 @@ const Map = (props) => {
     }
 
     const fireTorpedoPlayer = (event, row, cell) => {
-        removePreview(event);
         let map = props.enemy.map;
-        if (map[row][cell] === 1)   map[row][cell] = 2;
-        else if (map[row][cell] === 2 || map[row][cell] === 3)  return;
-        else map[row][cell] = 3;
+        if (map[row][cell] === 1){
+            map[row][cell] = 2;
+            event.target.classList.add('green');
+            event.target.style.backgroundColor = "green";
+        }
+        else if (map[row][cell] === 2 || map[row][cell] === 3){
+            return;
+        } else {
+            map[row][cell] = 3;
+            event.target.classList.add('red');
+            event.target.style.backgroundColor = "red";
+        }
 
         props.setEnemy(Object.assign(props.enemy, map));
-        props.setTurn(!props.turn)
+
+        setTimeout(() => {
+            props.setTurn(!props.turn);
+        },2000);
 
         let win = checkWin(map);
         if (win) {
@@ -152,12 +167,13 @@ const Map = (props) => {
         }
     };
 
-    const fireTorpedoIA = (row, cell) => {
+    const fireTorpedoIA = (event, row, cell) => {
         let map = props.enemy.map;
         let result = '';
         if (map[row][cell] === 1) {
             map[row][cell] = 2;
             result = 'hit';
+            event.target.style.backgroundColor = "green";
         }
         else if (map[row][cell] === 2 || map[row][cell] === 3)  return 'repeat';
         else {
@@ -166,7 +182,9 @@ const Map = (props) => {
         }
 
         props.setEnemy(Object.assign(props.enemy, map));
-        props.setTurn(!props.turn)
+        setTimeout(() => {
+            props.setTurn(!props.turn);
+        },2000);
 
         let win = checkWin(map);
         if (win) {
